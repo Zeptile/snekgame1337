9b5a4dbd4a884527d9af37a1a0bf4e09eb97d63a
Zeptile <gbf@teksavvy.com>
2019-12-06 13:19:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# snekgame1337
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/snekgame1337)
---
+++ index.ts
@@ -0,0 +1,151 @@
+// Import stylesheets
+import './style.css';
+
+const nodeWidth = 15;  // px
+const gridSize = 30;   // unit
+
+let direction: string; // Arrows -> [ArrowUp, ArrowDown, ArrowRight, ArrowLeft]
+let grid: number[][];
+
+const gameWindow: HTMLElement = document.querySelector('.game-window');
+const resetBtn: HTMLElement = document.querySelector('.reset');
+const coordsDebug: HTMLElement = document.querySelector('.coords')
+
+resetBtn.addEventListener('click', startup);
+document.addEventListener('keydown', (e) => direction = e.code);
+
+startup();
+
+function startup(): void {
+  grid = Array.from(Array(gridSize).fill(0), () => new Array(gridSize).fill(0));
+
+  gameWindow.style.width = `${gridSize * nodeWidth}px`;
+  gameWindow.style.height = `${gridSize * nodeWidth}px`;
+
+  drawPois();
+  
+  const ranCoords = getRandomCoords();
+  let snake = drawSnake(ranCoords.x, ranCoords.y);
+
+  setInterval( () => {
+    if(snake) {
+      clearSnake();
+
+      console.log(snake);
+
+      switch(direction) {
+        case 'ArrowUp':
+          snake = drawSnake(snake.x - 1, snake.y);
+          break;
+        case 'ArrowDown':
+          snake = drawSnake(snake.x + 1, snake.y);
+          break;
+        case 'ArrowLeft':
+          snake = drawSnake(snake.x, snake.y - 1);
+          break;
+        case 'ArrowRight':
+          snake = drawSnake(snake.x, snake.y + 1);
+          break;
+      }
+    }
+  }, 100)
+}
+
+function drawSnake(x: number, y: number): {x: number, y: number}  {
+
+  if (x > -1 && y > -1 && x < gridSize && y < gridSize) {
+    const currentNodeValue = grid[x][y];
+
+    if (currentNodeValue === 1) {
+      console.error('YOU HIT YOURSELF IDIOT');
+    }
+
+    if (currentNodeValue === 2) {
+      drawPois();
+    }
+
+    printCoords(x, y);
+
+    grid[x][y] = 3;
+
+    drawGrid(grid);
+
+    return {x: x, y: y};
+  }
+  
+  console.error('OUT_OF_BOUNDS');
+}
+
+function clearSnake(): void {
+  for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
+      if (grid[x][y] === 3) {
+        grid[x][y] = 0;
+      }
+    }
+  }
+}
+
+function drawPois(): void {
+  const coords = getRandomCoords();
+  grid[coords.x][coords.y] = 2;
+  drawGrid(grid);
+}
+
+function drawGrid(twoDimensionalArray: number[][]): void {
+
+  gameWindow.removeChild(gameWindow.firstChild); // Reset Grid
+
+  let table = document.createElement('table');
+  let tableBody = document.createElement('tbody');
+
+  twoDimensionalArray.forEach((rowData) => {
+    let row = document.createElement('tr');
+
+    rowData.forEach((cellData) => {
+      let cell = document.createElement('td');
+
+      switch(cellData) {
+        case 1:
+          cell.className = 'snake';
+          break;
+        case 2:
+          cell.className = 'pois';
+          break;
+        case 3:
+          cell.className = 'snake-head';
+          break;
+      }
+
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+
+  gameWindow.appendChild(table);
+}
+
+function printCoords(x: number, y: number) {
+  if (coordsDebug.firstChild) {
+    coordsDebug.removeChild(coordsDebug.firstChild);
+  }
+
+  let txt = document.createTextNode(`(${x}, ${y})`);
+  coordsDebug.appendChild(txt);
+}
+
+function getRandomCoords(): {x: number, y: number} {
+  const ranX = getRandomInt(0, gridSize - 1);
+  const ranY = getRandomInt(0, gridSize - 1);
+
+  return {x: ranX, y: ranY};
+}
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}

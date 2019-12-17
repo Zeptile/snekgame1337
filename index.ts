9eef5af47d412f08ec0469b1cecfd0d7ce445d2c
Zeptile <gbf@teksavvy.com>
2019-12-17 16:39:56 -0500

    Added FireBase  Leaderboard

--- index.ts
+++ index.ts
@@ -1,9 +1,33 @@
 // Import stylesheets
 import './style.css';
+import * as firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCKlt0LC2CnKA2qnf96uP2Gu2F0uOGQ1SU",
+  authDomain: "snekgame1337.firebaseapp.com",
+  databaseURL: "https://snekgame1337.firebaseio.com",
+  projectId: "snekgame1337",
+  storageBucket: "snekgame1337.appspot.com",
+  messagingSenderId: "980368520835",
+  appId: "1:980368520835:web:90b3ffa4fe9110037f4db6",
+  measurementId: "G-9S7FYBZJWQ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
 
 interface Coords {
   x: number;
   y: number;
+}
+
+interface Player {
+  name: string;
+  score: number;
 }
 
 enum Direction {
@@ -15,10 +39,10 @@ enum Direction {
 }
 
 enum EntityType {
-  nothing = 0,
-  snake = 1,
-  pois = 2,
-  snakehead = 3
+  Nothing = 0,
+  Snake = 1,
+  Pois = 2,
+  Snakehead = 3
 }
 
 const nodeWidth = 15;  // px
@@ -33,12 +57,19 @@ let gameover = false;
 
 const gameWindow: HTMLElement = document.querySelector('.game-window');
 const resetBtn: HTMLElement = document.querySelector('.reset');
-const coordsDebug: HTMLElement = document.querySelector('.coords')
+const coordsDebug: HTMLElement = document.querySelector('.coords');
+const scoreElement: HTMLElement = document.querySelector('.score');
+const postScoreElement: HTMLElement = document.querySelector('.post-score');
+const name: HTMLElement = document.querySelector('.name');
+const submitScoreBtn: HTMLElement = document.querySelector('.submit-score');
+const tbody: HTMLElement = document.querySelector('.tbody');
 
 resetBtn.addEventListener('click', startup);
 document.addEventListener('keydown', (e) => direction = e.code);
+submitScoreBtn.addEventListener('click', postScore);
 
 startup();
+drawLeaderboard();
 
 function startup(): void {
   grid = Array.from(Array(gridSize).fill(0), () => new Array(gridSize).fill(0));
@@ -47,6 +78,7 @@ function startup(): void {
   tailLength = 0;
   tail = [];
   gameover = false;
+  postScoreElement.style.display = 'none';
 
   if (intervalHandle) {
     window.clearInterval(intervalHandle);
@@ -89,18 +121,18 @@ function drawSnake(nextSnake: Coords, currentSnake: Coords): Coords {
 
     printCoords({x: nextSnake.x, y: nextSnake.y});
 
-    grid[nextSnake.x][nextSnake.y] = 3;
+    grid[nextSnake.x][nextSnake.y] = EntityType.Snakehead;
 
     if (currentSnake) {
       drawSnakeTail(nextSnake);
     }
 
-    if (nextNodeValue === 1) {
+    if (nextNodeValue === EntityType.Snake) {
       console.error('YOU HIT YOURSELF IDIOT');
       gameOver();
     }
 
-    if (nextNodeValue === 2) {
+    if (nextNodeValue === EntityType.Pois) {
       tailLength += 1;
       drawPois();
     }
@@ -111,7 +143,7 @@ function drawSnake(nextSnake: Coords, currentSnake: Coords): Coords {
     
     if (tail.length > tailLength) {
       let coords = tail[0];
-      grid[coords.x][coords.y] = 0;
+      grid[coords.x][coords.y] = EntityType.Nothing;
       tail.shift();
     }
 
@@ -142,15 +174,17 @@ function clearSnake(): void {
 
 function drawPois(): void {
   let coords = getRandomCoords();
-  while (grid[coords.x][coords.y] !== 0) {
+  while (grid[coords.x][coords.y] !== EntityType.Nothing) {
     coords = getRandomCoords();
   }
   grid[coords.x][coords.y] = 2;
+  printScore();
   drawGrid(grid);
 }
 
 function gameOver(): void {
   gameover = true;
+  postScoreElement.style.display = 'block';
   window.clearInterval(intervalHandle);
   gameWindow.removeChild(gameWindow.firstChild);
   let gameoverText = document.createElement('h1');
@@ -173,13 +207,13 @@ function drawGrid(twoDimensionalArray: number[][]): void {
       let cell = document.createElement('td');
 
       switch(cellData) {
-        case 1:
+        case EntityType.Snake:
           cell.className = 'snake';
           break;
-        case 2:
+        case EntityType.Pois:
           cell.className = 'pois';
           break;
-        case 3:
+        case EntityType.Snakehead:
           cell.className = 'snake-head';
           break;
       }
@@ -204,11 +238,80 @@ function printCoords(coords: Coords) {
   coordsDebug.appendChild(txt);
 }
 
+function printScore() {
+  if (scoreElement.firstChild) {
+    scoreElement.removeChild(scoreElement.firstChild);
+  }
+
+  let txt = document.createTextNode(`${tailLength}`);
+  scoreElement.appendChild(txt);
+}
+
 function getRandomCoords(): Coords {
   const ranX = getRandomInt(0, gridSize - 1);
   const ranY = getRandomInt(0, gridSize - 1);
 
   return {x: ranX, y: ranY};
+}
+
+function drawLeaderboard(): void {
+
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  db.collection("players").get().then((querySnapshot) => {
+  let players: Player[] = [];
+  querySnapshot.forEach((doc) => {
+    let player = doc.data();
+    players.push({
+      name: player.name,
+      score: player.score,
+    });
+  });
+
+  console.log(players);
+
+  players.sort((s1, s2) => (s1.score < s2.score) ? 1 : -1);
+  players = players.slice(0, 10);
+  let count = 0;
+
+  players.forEach((player) => {
+    count++;
+    let row = document.createElement('tr');
+
+    let positionCell = document.createElement('td');
+    positionCell.innerText = count.toString();
+    positionCell.className = 'leaderboard-node';
+    row.appendChild(positionCell);
+    let nameCell = document.createElement('td');
+    nameCell.innerText = player.name
+    nameCell.className = 'leaderboard-node';
+    row.appendChild(nameCell);
+    let scoreCell = document.createElement('td');
+    scoreCell.innerText = player.score.toString()
+    scoreCell.className = 'leaderboard-node';
+    row.appendChild(scoreCell);
+
+    tbody.appendChild(row);
+  });
+});
+}
+
+function postScore(): void {
+  db.collection("players").add({
+    name: name.value,
+    score: tailLength
+  }).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      postScoreElement.style.display = 'none';
+      drawLeaderboard();
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+      postScoreElement.style.display = 'none';
+      drawLeaderboard();
+  });
 }
 
 function getRandomInt(min: number, max: number): number {
